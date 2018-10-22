1ebd582b546aab64ffcd011d6ff9a48f13695f8b
Bruno Logerfo <bclogerfo@hotmail.com>
2018-10-22 12:43:38 -0300

    new commands: pull and push new configurations: pullArgs and pushArgs

--- src/extension.ts
+++ src/extension.ts
@@ -4,35 +4,56 @@ export function activate(context: vscode.ExtensionContext) {
     //proposed API
     //(<any>vscode.window).onDidChangeActiveTerminal(e => lastActiveTerminal = e);
     context.subscriptions.push(vscode.commands.registerCommand('cd-repo.cd', cd));
+    context.subscriptions.push(vscode.commands.registerCommand('cd-repo.pull', pull));
+    context.subscriptions.push(vscode.commands.registerCommand('cd-repo.push', push));
 }
 
 //var lastActiveTerminal: vscode.Terminal;
-async function cd(args) {
-    const config = vscode.workspace.getConfiguration("cd-repo", args._fsPath);
+function getTerminal(config) {
     const terminalType: string = config.get('terminal', "AlwaysCreate");
-    const command = config.get('command', 'cd');
-    let terminal: vscode.Terminal;
     switch (terminalType) {
-        case "AlwaysCreate":
-            terminal = vscode.window.createTerminal();
-            break;
+        case "AlwaysCreate": return vscode.window.createTerminal();
 
-        case "First":
-            terminal = vscode.window.terminals.length > 0 ? vscode.window.terminals[0] : vscode.window.createTerminal();
-            break;
+        case "First": return vscode.window.terminals.length > 0 ? vscode.window.terminals[0] : vscode.window.createTerminal();
 
-        case "Last":
-            terminal = vscode.window.terminals.length > 0 ? vscode.window.terminals[vscode.window.terminals.length - 1] : vscode.window.createTerminal();
-            break;
+        case "Last": return vscode.window.terminals.length > 0 ? vscode.window.terminals[vscode.window.terminals.length - 1] : vscode.window.createTerminal();
 
-        //case "LastActive":
-        //    terminal = lastActiveTerminal ? lastActiveTerminal : vscode.window.createTerminal();
-        //    break;
+        //case "LastActive": return lastActiveTerminal ? lastActiveTerminal : vscode.window.createTerminal();
 
         default: vscode.window.showErrorMessage(`${terminalType} is not a valid terminal option.`);
     }
+}
+
+function send(terminal: vscode.Terminal, text: string) {
     terminal.show();
-    terminal.sendText(`${command} "${(<any>args).rootUri.fsPath}"`);
+    terminal.sendText(text);
+}
+
+async function cd(args) {
+    const config = vscode.workspace.getConfiguration("cd-repo", args._fsPath);
+    const command = config.get('command', 'cd');
+    let terminal: vscode.Terminal = getTerminal(config);
+    if (terminal) {
+        send(terminal, `${command} "${(<any>args).rootUri.fsPath}"`);
+    }
+}
+
+async function pull(args) {
+    const config = vscode.workspace.getConfiguration("cd-repo", args._fsPath);
+    let terminal: vscode.Terminal = getTerminal(config);
+    const pullArgs = config.get('pullArgs', '');
+    if (terminal) {
+        send(terminal, `git pull ${pullArgs}`);
+    }
+}
+
+async function push(args) {
+    const config = vscode.workspace.getConfiguration("cd-repo", args._fsPath);
+    let terminal: vscode.Terminal = getTerminal(config);
+    const pushArgs = config.get('pushArgs', '');
+    if (terminal) {
+        send(terminal, `git push ${pushArgs}`);
+    }
 }
 
 export function deactivate() {
